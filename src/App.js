5e7057f3c5a393f39b38041b3dcc430804893696
Morris <manthony9@myseneca.ca>
2021-01-01 17:59:14 -0500

    update routing with react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+import {Switch,Route} from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page!</h1>
+  </div>
+)
+
+const TopicList = () => (
+  <div>
+    <h1>Topic List Page</h1>
+  </div>
+)
+
+const TopicDetail = () => (
+  <div>
+    <h1>Topic Detail Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
