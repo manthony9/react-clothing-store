0eee8d8b1a8a47d60aae509355d0b0ec53aea95f
Morris <manthony9@myseneca.ca>
2021-01-17 00:36:57 -0500

    updated cart details and quantity function

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN : 'TOGGLE_CART_HIDEDN'
+    TOGGLE_CART_HIDDEN : 'TOGGLE_CART_HIDEDN',
+    ADD_ITEM:'ADD_ITEM'
 }
 
 export default CartActionTypes;
